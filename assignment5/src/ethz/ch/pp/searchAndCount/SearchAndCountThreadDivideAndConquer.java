339bd63e9a579216031be3f8a674d866d0088056
jstimolo <jstimolo@ethz.ch>
2021-05-01 15:24:13 +0200

    New idea: - Make a DATASTRUCTURE to divide the array (how in ass 2) - Assign the THREADS to the subs of the array (FOR-LOOP) - Make the threads start from the method and create method run (@override) - in run: divide and conquer

--- assignment5/src/ethz/ch/pp/searchAndCount/SearchAndCountThreadDivideAndConquer.java
+++ assignment5/src/ethz/ch/pp/searchAndCount/SearchAndCountThreadDivideAndConquer.java
@@ -3,57 +3,14 @@ package ethz.ch.pp.searchAndCount;
 import ethz.ch.pp.util.Workload;
 
 public class SearchAndCountThreadDivideAndConquer extends Thread {
-	public static int[] list;
-	public static Workload.Type wlt;
-	public static int cutoff,hi,lo,result;
-	
-	
-	SearchAndCountThreadDivideAndConquer(int hi, int lo){
-		this.lo = lo;
-		this.hi = hi;
-	}
 	
 	public static int countNoAppearances(int[] input, Workload.Type wt, int cutOff, int numThreads) {
-		list = input;
-		wlt = wt;
-		cutoff = cutOff;
-		hi = input.length-1;
-		lo = 0;
-		return result;
+		SearchAndCountThreadDivideAndConquer t = new SearchAndCountThreadDivideAndConquer();
+		System.out.println("hello");
+		return 0;
 	}
 	
 	
-	public void run(){
-		boolean heavy = (Workload.doWork(list[lo], wlt));
-		int size = hi-lo;
-		
-		if (size < cutoff) {
-			if(!heavy) {
-				result = 0;
-				return;
-			} else {
-				result = 1;
-				return;
-			}
-		} 
-		
-		int mid = size/2;
-		SearchAndCountThreadDivideAndConquer t1 = new SearchAndCountThreadDivideAndConquer(lo,mid);
-		SearchAndCountThreadDivideAndConquer t2 = new SearchAndCountThreadDivideAndConquer(mid+1,hi);
-		
-		t1.start();
-		t2.start();
-		
-		try {
-			t1.join();
-			t2.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		result = t1.result + t2.result;
-		return;
-	}
+
 
 }
